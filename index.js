f8b059a0265910b5d8cdfbe9a2ca342034e54d4f
Justin Staubach <justin@staubach.us>
2019-12-13 11:21:34 -0500

    -

--- index.js
+++ index.js
@@ -7,14 +7,18 @@ async function run() {
   const myToken = process.env.ACTION_TOKEN ? process.env.ACTION_TOKEN : core.getInput("action-token");
   const commentText = process.env.COMMENT_TEXT ? process.env.COMMENT_TEXT : core.getInput("comment-text");
   //const octokit = new github.GitHub(myToken);
-  const octokit = new graphql.GitHub(myToken);
+  const graphqlWithAuth = graphql.defaults({
+    headers: {
+      authorization: `token secret123`
+    }
+  });
   const context = github.context;
   
   console.log(
     `>> Action triggered by issue #${context.issue.number}\n`,
     `   << Comment on issue with a greeting: "${commentText}"`,
   );
-  await actions.githubProjects.addComment(octokit, context, commentText);
+  await actions.githubProjects.addComment(graphqlWithAuth, context, commentText);
 }
 
 run()
