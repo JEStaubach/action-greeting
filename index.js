b22a4561b48c812ae7d7fc01608d94bf6d66e3a3
Justin Staubach <justin@staubach.us>
2019-12-13 11:11:58 -0500

    -

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const actions = require("@jestaubach/actions");
 async function run() {
   const myToken = process.env.ACTION_TOKEN ? process.env.ACTION_TOKEN : core.getInput("action-token");
   const commentText = process.env.COMMENT_TEXT ? process.env.COMMENT_TEXT : core.getInput("comment-text");
-  const octokit = new github.GitHub(myToken);
+  //const octokit = new github.GitHub(myToken);
+  const octokit = new graphql.GitHub(myToken);
   const context = github.context;
   
   console.log(
